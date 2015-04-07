eaf8a1371d535aae14136cdbdf040179cbba9b41
jonacha <jonacha@opedeusto.es>
2015-04-07 18:27:50 +0200

    Coste uniforme de bread

--- src/es/deusto/ingenieria/is/search/touring/MainProgram.java
+++ src/es/deusto/ingenieria/is/search/touring/MainProgram.java
@@ -6,6 +6,7 @@ import es.deusto.ingenieria.is.search.algorithms.blind.DepthFSwithLog;
 import es.deusto.ingenieria.is.search.algorithms.heuristic.BestFS;
 import es.deusto.ingenieria.is.search.algorithms.heuristic.BestFSwithLog;
 import es.deusto.ingenieria.is.search.touring.formulacion.TouringProblem;
+import es.deusto.ingenieria.is.search.touring.formulacion.heuristics.CosteUniforme;
 import es.deusto.ingenieria.is.search.touring.formulacion.heuristics.EvaluacionDeMovimientos;
 
 public class MainProgram {
@@ -24,15 +25,15 @@ public class MainProgram {
 			TouringProblem problem = new TouringProblem();			
 			problem.addInitialState(problem.gatherInitialPercepts());
 			
-			problem.solve(DepthFS.getInstance()); // resuelve el recorrido sin registrar los datos en un txt algoritmo Depth First.
+//			problem.solve(DepthFS.getInstance()); // resuelve el recorrido sin registrar los datos en un txt algoritmo Depth First.
 	//		problem.solve(DepthFSwithLog.getInstance()); // resuelve el recorrido registrando los datos en un txt algoritmo Depth First.
 	
-			problem.solve(BreadthFS.getInstance()); // resuelve el recorrido sin registrar los datos en un txt algoritmo Breadth First.
+//			problem.solve(BreadthFS.getInstance()); // resuelve el recorrido sin registrar los datos en un txt algoritmo Breadth First.
 	//		problem.solve(BreadthFSwithLog.getInstance()); // resuelve el recorrido registrando los datos en un txt algoritmo Breadth First.
 			
-			problem.solve(new BestFS(new EvaluacionDeMovimientos())); // resuelve el recorrido sin registrar los datos en un txt algoritmo Best First.
+//			problem.solve(new BestFS(new EvaluacionDeMovimientos())); // resuelve el recorrido sin registrar los datos en un txt algoritmo Best First.
 	//		problem.solve(new BestFSwithLog(new EvaluacionDeMovimientos())); // resuelve el recorrido registrando los datos en un txt algoritmo Best First.
-			
+		problem.solve(CosteUniforme.getInstance());	
 		} catch (Exception ex) {
 			System.err.println("% [Main Program] Error: " + ex.getMessage());
 			ex.printStackTrace();
